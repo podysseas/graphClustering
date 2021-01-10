c03858ad80a6863fbbf3158f73a442336d4cd049
podysseas <podysseas@auth.gr>
2021-01-11 01:42:38 +0200

    Add for B1 , T1 ,S1 , fixing folder # automation fix

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Sep 19 17:10:24 2020
 
@@ -27,9 +26,10 @@ import datetime
 # from sklearn.neighbors import kneighbors_graph
 # from sklearn.cluster import SpectralClustering
 
+    
 def Calculate_distance_metric_inside_cluster(distance,clusters):
     
-    clusters = np.array(clust_fastgreedy).tolist()
+    clusters = np.array(clusters).tolist()
     
     average_cluster = np.zeros(shape =len(clusters))
     variance_cluster = np.zeros(shape =len(clusters))
@@ -153,6 +153,7 @@ def Plot_Distribution(distance):
     '''
     A = distance.flatten()
     
+    A = np.log10(A*10)
     # t = sns.distplot(A,hist=True, kde=True,
     #                  bins=int(300 / 5), color="darkblue",
     #                  hist_kws={'edgecolor': 'black'},
@@ -202,7 +203,8 @@ def Choose_Threshold_Graph(distance, threshold):
 
     np.fill_diagonal(adj_matrix, 0)  # reject self - loops
 
-    graph_threshold = ig.Graph.Adjacency((distance > threshold).tolist())  # create a graph from adjacency matrix
+    graph_threshold = ig.Graph.Adjacency((distance > threshold).tolist())  
+    # create a graph from adjacency matrix
 
     return graph_threshold
 
@@ -294,150 +296,197 @@ def shared_nearest_neighbors(adj_matrix_boolean,threshold_neighbors):
 
 if __name__ == "__main__":
     
-    FILE = '2_IMGT-gapped-nt-sequences.txt'
-    #FILE = '4_IMGT-gapped-AA-sequences.txt'
-    OUTPUT_FOLDER = "MY_IMAGES/"
-    COLUMN = 'V.D.J.REGION'
-    SEQUENCE_NUMBER = 'Sequence.number'
-    
-    path = os.getcwd()
-    e = datetime.datetime.now()
-    Hour_time_name = str(e.hour) + "_" + str(e.minute) + "_" +  str(e.second)
-    os.makedirs("images_"+ Hour_time_name)
-    OUTPUT_FOLDER = "images_"+ Hour_time_name +"/"
+    SAMPLES = ["B1",'S1',"T1"]
     
     
-    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    data = pd.read_csv(os.path.join(__location__, FILE), sep='\t')
-    filtered_data = data[['Sequence.number', COLUMN]].dropna()  # drop nan
-    #filtered_data = data[['Sequence ID',COLUMN]].head(2000)
-
-    ''' this block calculates distance '''
-    start = time.time()
-    distance = calculate_distance(filtered_data)
+    for SAMPLE in SAMPLES:
+        FILE = '2_IMGT-gapped-nt-sequences_' + SAMPLE + '.txt'
     
     
-    ''' ***************************    '''
-    layout              ="kk"
-
-    #--------------------------------------------------------
-    figure,A = Plot_Distribution(distance)
-    figure.savefig(OUTPUT_FOLDER +'distribution_of_similarities.jpg', dpi=400)
+        COLUMN = 'V-D-J-REGION'
+        SEQUENCE_NUMBER = 'Sequence number'
+        
+        path = os.getcwd()
+        e = datetime.datetime.now()
+        Hour_time_name = str(e.hour) + "_" + str(e.minute) + "_" +  str(e.second)
+        
+        Day_month = str(e.day) + "_"  + str(e.month) + "_"
+        
     
+        OUTPUT_FOLDER = SAMPLE
+        OUTPUT_FOLDER_IMAGES = SAMPLE + "/IMAGES/"
     
-    #--------------------------------------------------------
-    graph_threshold = Choose_Threshold_Graph(distance, threshold=0.86)
-   
-    ig.plot(graph_threshold, OUTPUT_FOLDER + "graph_threshold.png", layout=layout, bbox=(500, 500), margin=50, inline='None')
-
-    #--------------------------------------------------------
-    graph_normalized = normalized_weighted_graph(distance)
+        #os.makedirs(OUTPUT_FOLDER)
+        f_m = open(path + "\\" + OUTPUT_FOLDER + "\\modularities_" + SAMPLE + ".txt", "w")
+        
+        
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        data = pd.read_csv(os.path.join(__location__, FILE), sep='\t')
+        filtered_data = data[[SEQUENCE_NUMBER, COLUMN]].sample(n=500).dropna()# drop nan # take 500 random samples
+        
     
-    ig.plot(graph_normalized,OUTPUT_FOLDER + "graph_normalized.png", layout=layout, bbox=(500, 500), margin=50, inline='None')
+        ''' this block calculates distance '''
+        start = time.time()
+        distance = calculate_distance(filtered_data)
+        
+        
+        ''' ***************************    '''
+        
+        
+        ''' this block calculates the distribution of similarities '''
+        
     
-    #--------------------------------------------------------
-    temp_graph          = graph_normalized
-    adj_matrix_boolean  = np.array(temp_graph.get_adjacency().data)
-    threshold_neighbors = 300
-    graph_shared,label  = shared_nearest_neighbors(adj_matrix_boolean,threshold_neighbors)
-    Plot_Graph(graph_shared,label,OUTPUT_FOLDER +"graph_shared.png",layout)
-    #ig.plot(graph_shared, "graph_shared.png", layout=layout, bbox=(500, 500), margin=50, inline='None')
-    #--------------------------------------------------------
+        figure,A = Plot_Distribution(distance)
+        
+        figure.savefig(OUTPUT_FOLDER +'/distribution_of_similarities.jpg', dpi=400)
+        
+        ''' ***************************    '''
     
+        #--------------------------------------------------------
+        layout = "kk"
+        
+        ''' This block produces the normalized graph '''
     
-    '''
-        TO DO : 
-            - check the algorithms
+        graph_normalized = normalized_weighted_graph(distance)
             
-            Walktrap  --- checked
+        ig.plot(graph_normalized,OUTPUT_FOLDER + "graph_normalized.png", 
+                                    layout=layout, bbox=(500, 500), margin=50, inline='None')
+        
+        ''' ***************************    '''
+    
+        
+        temp_graph          = graph_normalized # check if take normalized or graph threshold
+        adj_matrix_boolean  = np.array(temp_graph.get_adjacency().data)
+        threshold_neighbors = 300
+        graph_shared,label  = shared_nearest_neighbors(adj_matrix_boolean,threshold_neighbors)
             
-            Fastgreedy -- checked
+            
+        Plot_Graph(graph_shared,label,OUTPUT_FOLDER + "graph_normalized_shared.png",layout)
+       
     
-    ''' 
-    g = graph_threshold
-    g = add_weights_as_list(g,distance)
-    wtrap = g.community_walktrap(weights = g.es['weight'],steps = 30)
-    lab =np.array(wtrap.as_clustering()).tolist()
-    clust_wtrap =  wtrap.as_clustering()
-    labels_wtrap = Transform_labels_2dList_to_intArray(distance,lab)
-    Plot_Graph(g,labels_wtrap,OUTPUT_FOLDER +"clustering_wtrap_layout_kk.png",layout)
+        ''' this block produces the analysis for each threshold '''
+        
+        threshold = np.array([0.70,0.75,0.80,0.85,0.90,0.95])
+        
+        for i in range(0,len(threshold[:])):
+            
+            
+            graph_threshold = Choose_Threshold_Graph(distance,  threshold[i])
+    
+            threshold_name  = "threshold_" + str(threshold[i]) + "_"
+        
+            # ig.plot(graph_threshold, OUTPUT_FOLDER + threshold_name + "graph_threshold.png",
+            #                         layout=layout, bbox=(500, 500), margin=50, inline='None')
+        
+            #--------------------------------------------------------
+            temp_graph          = graph_threshold # check if take normalized or graph threshold
+            adj_matrix_boolean  = np.array(temp_graph.get_adjacency().data)
+            threshold_neighbors = 300
+            graph_shared,label  = shared_nearest_neighbors(adj_matrix_boolean,threshold_neighbors)
+            # Plot_Graph(graph_shared,label,OUTPUT_FOLDER + threshold_name + "graph_shared.png",layout)
+    
+            #--------------------------------------------------------
+            
+            
+            # g = graph_threshold
+            # g = add_weights_as_list(g,distance)
+            # wtrap = g.community_walktrap(weights = g.es['weight'],steps = 30)
+            # lab =np.array(wtrap.as_clustering()).tolist()
+            # clust_wtrap =  wtrap.as_clustering()
+            # labels_wtrap = Transform_labels_2dList_to_intArray(distance,lab)
+            # avg_clust_wtrap,var_clust_wtrap =Calculate_distance_metric_inside_cluster(distance,clust_wtrap)
+    
+            # Plot_Graph(g,labels_wtrap,OUTPUT_FOLDER + threshold_name + "clustering_wtrap_layout_kk.png",layout)
+            # Plot_graph_visual_style(clust_wtrap,OUTPUT_FOLDER + threshold_name + "clustering_wtrap_visual_style.png")
+    
+            
+            g = graph_threshold
+            g.to_undirected()
+            g = add_weights_as_list(g,distance)
+            fastgreedy = g.community_fastgreedy(weights = g.es['weight'])    
+            lab =np.array(fastgreedy.as_clustering()).tolist()
+            clust_fastgreedy =  fastgreedy.as_clustering()
+            labels_fastgreedy = Transform_labels_2dList_to_intArray(distance,lab)
+            avg_clust_fastgreedy,var_clust_fastgreedy = Calculate_distance_metric_inside_cluster(distance, clust_fastgreedy)
+            
+            Plot_Graph(g,labels_fastgreedy,OUTPUT_FOLDER_IMAGES +  threshold_name + "clustering_fastgreedy_layout_kk.png",layout)
+            Plot_graph_visual_style(clust_fastgreedy,OUTPUT_FOLDER_IMAGES +  threshold_name + "clustering_fastgreedy_visual_style.png")
+        
+        
+            # layout = g.layout_drl(weights=g.es["weight"], fixed=None, seed=None, options=None, dim=2)
+        
+            # Plot_Graph(g,labels_fastgreedy,OUTPUT_FOLDER + threshold_name + "clustering_test.png",layout)
+        
+            f_m = open(path + "\\" + OUTPUT_FOLDER + "\\modularities_" + SAMPLE + ".txt", "a")
+            f_m.write("modularity for fast_greedy " +  threshold_name + "      " + str(clust_fastgreedy.modularity) +" \n")
+            #f_m.write("average clust  similarity " +  threshold_name + str(np.round(avg_clust_fastgreedy,decimals=4)) +" \n")
+            #f_m.write("variance clust similarity  " +  threshold_name + str(np.round(var_clust_fastgreedy,decimals=4))+" \n")
+            f_m.write("--------------------------------------------------------------- \n \n")
+        
+        
+        
+        end = time.time()
+        elapsed_time = format(end - start, '.3f')
+        print('The execution time is {0} seconds\n'.format(elapsed_time))
+        
+        
+        A = distance.flatten()
+        mean_sim  = statistics.fmean(A)
+        median_sim = statistics.median(A)
+        pstdev_sim = statistics.pstdev(data = A , mu = mean_sim)
+        
+        percentile_50_sim = np.percentile(A,50)   
+        percentile_75_sim = np.percentile(A,75)
+        
+        f = open(path + "\\" + OUTPUT_FOLDER + "\\similarity_metrics_" + SAMPLE + ".txt", "w")
+        
+        f.write("Some similarity metrics here \n---------------------------- \n")
+        f.write("mean_sim  :  " + str(mean_sim) + " \n")
+        f.write("median_sim  :  " + str(median_sim) +" \n")
+        f.write("pstdev_sim  :  " + str(pstdev_sim) + "\n")
+        f.write("percentile-50_sim  :  " + str(percentile_50_sim) + "\n")
+        f.write("percentile-75_sim  :  " + str(percentile_75_sim) + "\n")
+    
+    
+        f_m.close()
+        f.close()
+        
+        
+         
+        # g = graph_threshold
+        # g.to_undirected()
+        # g = add_weights_as_list(g,distance)
+        # clust_lead_eig = g.community_leading_eigenvector(clusters = 5 ,weights = g.es['weight'])    
+    
+    
+        # g = graph_threshold
+        # g.to_undirected()
+        # g = add_weights_as_list(g,distance)
+        # clust_optimal= g.community_optimal_modularity(weights = g.es['weight'])    
+    
+        # g = graph_threshold
+        # g.to_undirected()
+        # g = add_weights_as_list(g,distance)
+        # clust_multi_level= g.community_multilevel(weights = g.es['weight'],return_levels=True) 
+        #10 clusters
+    
+    
+        #matrix_to_spectral = graph_normalized.get_adjacency()[:, :]  # int 0 1
+        # matrix_to_spectral = graph_threshold.get_adjacency()[:, :]  # int 0 1
+        
+        # matrix_to_spectral = np.array(matrix_to_spectral.data)  # int
+        
+        # d = np.diag(matrix_to_spectral.sum(axis=1))
+        
+        # l = np.array(d - matrix_to_spectral)
+        # eig_L, eig_vec_L = np.linalg.eig(l)
+        
+        # i = np.where(eig_L < 0.1)
+        
+        #sc               = SpectralClustering(5, affinity='precomputed', n_init=100,assign_labels='discretize')   
+        
+        #label           = sc.fit_predict(adjacency_matrix)
+                        
+        end = time.time()    
 
-
-    g = graph_threshold
-    g.to_undirected()
-    g = add_weights_as_list(g,distance)
-    fastgreedy = g.community_fastgreedy(weights = g.es['weight'])    
-    lab =np.array(fastgreedy.as_clustering()).tolist()
-    clust_fastgreedy =  fastgreedy.as_clustering()
-    labels_fastgreedy = Transform_labels_2dList_to_intArray(distance,lab)
-    
-    Plot_Graph(g,labels_fastgreedy,OUTPUT_FOLDER +"clustering_fastgreedy_layout_kk.png",layout)
-                          
-    
-    Plot_graph_visual_style(clust_fastgreedy,OUTPUT_FOLDER +"clustering_fastgreedy_visual_style.png")
-    Plot_graph_visual_style(clust_wtrap,OUTPUT_FOLDER +"clustering_wtrap_visual_style.png")
-
-
-    layout = g.layout_drl(weights=g.es["weight"], fixed=None, seed=None, options=None, dim=2)
-
-    Plot_Graph(g,labels_fastgreedy,OUTPUT_FOLDER +"clustering_test.png",layout)
-
-    
-    avg_clust_fastgreedy,var_clust_fastgreedy = Calculate_distance_metric_inside_cluster(distance, clust_fastgreedy)
-    avg_clust_wtrap,var_clust_wtrap =Calculate_distance_metric_inside_cluster(distance,clust_wtrap)
-      
-    
-    Plot_graph_visual_style(clust_fastgreedy,OUTPUT_FOLDER +"clustering_fastgreedy_visual_style.png")
-    Plot_graph_visual_style(clust_wtrap,OUTPUT_FOLDER +"clustering_wtrap_visual_style.png")
-
-
-    #matrix_to_spectral = graph_normalized.get_adjacency()[:, :]  # int 0 1
-    # matrix_to_spectral = graph_threshold.get_adjacency()[:, :]  # int 0 1
-    
-    # matrix_to_spectral = np.array(matrix_to_spectral.data)  # int
-    
-    # d = np.diag(matrix_to_spectral.sum(axis=1))
-    
-    # l = np.array(d - matrix_to_spectral)
-    # eig_L, eig_vec_L = np.linalg.eig(l)
-    
-    # i = np.where(eig_L < 0.1)
-    
-    #sc               = SpectralClustering(5, affinity='precomputed', n_init=100,assign_labels='discretize')   
-    
-    #label           = sc.fit_predict(adjacency_matrix)
-                    
-    end = time.time()
-    elapsed_time = format(end - start, '.3f')
-    print('The execution time is {0} seconds\n'.format(elapsed_time))
-    
-    
-    A = distance.flatten()
-    mean_sim  = statistics.fmean(A)
-    median_sim = statistics.median(A)
-    pstdev_sim = statistics.pstdev(data = A , mu = mean_sim)
-    
-    percentile_50_sim = np.percentile(A,50)   
-    percentile_75_sim = np.percentile(A,75)
-    
-    f = open(path +"/similarity_metrics.txt", "w")
-    
-    f.write("Some similarity metrics here \n ---------------------------- \n")
-    # f.close()
-
-    # f = open(path + "similarity_metrics.txt", "a")
-    
-    f.write("mean_sim  :  " + str(mean_sim) + " \n")
-    f.write("median_sim  :  " + str(median_sim+1) +" \n")
-    f.write("pstdev_sim  :  " + str(pstdev_sim) + "\n")
-    f.write("percentile-50_sim  :  " + str(percentile_50_sim) + "\n")
-    f.write("percentile-75_sim  :  " + str(percentile_75_sim) + "\n")
-
-    f.close()
-    
-    
-    
-    
-    
-    
     
